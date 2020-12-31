4df738651af792c4f540521c5b0654083001d9a1
gaojr <gaojr427@hotmail.com>
2020-12-31 11:18:29 +0800

    :tada: 项目初始化

---
+++ src/main/java/cn/gjr/test/json/IJson.java
@@ -0,0 +1,20 @@
+package cn.gjr.test.json;
+
+/**
+ * @author gaojr
+ */
+public interface IJson {
+
+    <B> String bean2String(B b);
+
+    <B> B string2Bean(String s, Class<B> clazz);
+
+    <O> String object2String(O o);
+
+    <O> O string2Object(String s);
+
+    <B, O> O bean2Object(B b);
+
+    <B, O> B object2Bean(O o, Class<B> clazz);
+
+}
